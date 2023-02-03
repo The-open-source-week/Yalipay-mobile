56d85b70bdcc91c66c7878faef68d9edec437ff2
dvs2000 <dorivaldodossantos2000@gmail.com>
2023-02-03 11:14:56 +0100

    Configs app and add some assets

---
+++ lib/src/utils/consts_utils.dart
@@ -0,0 +1,23 @@
+// IMAGES
+import 'package:flutter/material.dart';
+
+const handImage         = "assets/imgs/hand.png";
+const avatarImage       = "assets/imgs/avatar.png";
+const cloudCheckImage   = "assets/imgs/cloud-check.png";
+
+// ICONS
+const homeYellowIcon    = "assets/imgs/icons/home-yellow.png";
+const homeWhiteIcon     = "assets/imgs/icons/home-white.png";
+const folderYellowIcon  = "assets/imgs/icons/folder-yellow.png";
+const folderWhiteIcon   = "assets/imgs/icons/folder-white.png";
+const transferWhiteIcon = "assets/imgs/icons/transfer-white.png";
+const visaIcon          = "assets/imgs/icons/visa.png";
+
+// FONTS
+const fontPoppins = "poppins";
+const fontInter   = "inter";
+
+// COLORS
+const colorYellow  = Color(0xffFCEE21);
+const colorBlackBG = Color(0xff131313);
+
---
+++ lib/src/views/app_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:yalipay/src/utils/consts_utils.dart';
+
+class AppView extends StatelessWidget {
+  const AppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "YaliPay",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: colorYellow,
+        scaffoldBackgroundColor: colorBlackBG,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(fontFamily: fontPoppins),
+        )
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
